f6f742701818ebdeeb47dca084c3ff8705a612ee
Fabricio <fabricio.seb1@gmail.com>
2022-10-12 10:32:11 -0300

    fix: traveller - trying to fix prod typeorm connection/2
    Change-Id: I6308aee9695efa38690f66ab592c5e6826016219

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,12 @@ import swaggerDocs from './config/swagger';
 import config, { environments } from './config/config';
 import app from './app';
 
-if (config.env !== environments.PRODUCTION) {
-  app.listen(config.port, async () => {
+app.listen(process.env.PORT || config.port, async () => {
+  await database();
+
+  swaggerDocs(app, config.publicUrl, config.port);
+
+  if (environments.DEVELOPMENT)
     logger.info(`API rodando em http://${config.publicUrl}:${config.port}`);
-
-    await database();
-
-    swaggerDocs(app, config.publicUrl, config.port);
-  });
-} else {
-  app.listen(process.env.PORT, async () => {
-    logger.info(`API rodando em http://${config.publicUrl}`);
-
-    await database();
-
-    swaggerDocs(app, config.publicUrl, config.port);
-  });
-}
+  else logger.info(`API rodando em http://${config.publicUrl}`);
+});
